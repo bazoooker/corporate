f62786fe2aa3ef2b9a9de88996c6c65cfbf05a13
Alex <manakov55@yandex.ru>
2017-04-26 16:21:33 +0700

    gulpfile number 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,63 @@
+
+// переменные
 var gulp = require('gulp');
 // Requires the gulp-sass plugin
-var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
-	gulp.src('source/sass/**/*.scss') // Gets all files ending with .scss in app/scss
-    .pipe(sass())
-    .pipe(gulp.dest('public/styles'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "corporate"
+    });
+
+    gulp.watch("source/sass/**/*.scss", ['sass']);
+    gulp.watch("public/**/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('watch', function(){
-  gulp.watch('source/sass/**/*.scss', ['sass']); 
-  gulp.watch('public/*.html', ['sass']);
-}) 
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("source/sass/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("public/styles"))
+        .pipe(browserSync.stream());
+});
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'corporate'
-    },
-  })
-})
+gulp.task('default', ['serve']);
+
+
+
+
+
+
+
+
+
+
+// gulp.task('browserSync', function() {
+//   browserSync.init({
+//     server: {
+//       baseDir: 'corporate'
+//     }
+//   });
+// });
+
+
+// gulp.task('sass', function() {
+// 	return gulp.src('source/sass/**/*.scss') // Gets all files ending with .scss in app/scss
+//     .pipe(sass())
+//     .pipe(gulp.dest('public/styles'))
+//     .pipe(browserSync.stream({
+//       stream: true
+//     }))
+// });
+
+
+// gulp.task('watch', ['browserSync', 'sass'], function(){
+//   gulp.watch('public/*.html', browserSync.reload);
+//   gulp.watch('source/sass/**/*.scss', ['sass']);   
+// }); 
+
+// gulp.task('default', ['watch']);
